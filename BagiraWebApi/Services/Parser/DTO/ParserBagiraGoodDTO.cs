294eebc7b48df3b649860c0a134b4476fb166796
Danil HP <bogdanovdanila1404@gmail.com>
2024-02-24 18:00:08 +0500

    feat: add ParserBagiraGoodDTO

---
+++ BagiraWebApi/Services/Parser/DTO/ParserBagiraGoodDTO.cs
@@ -0,0 +1,12 @@
+﻿using BagiraWebApi.Models.Parser;
+
+namespace BagiraWebApi.Services.Parser.DTO
+{
+    public class ParserBagiraGoodDTO
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public decimal Price { get; set; }
+        public List<ParserGood> ParserGoods { get; set; } = null!;
+    }
+}
