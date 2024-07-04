d6ed329b44acbd073b99c7e6cad6f59a1ea2bd8f
DanilWork <bogdanovdanila1404@gmail.com>
2024-07-04 15:46:48 +0500

    feat: add clients to AuthConfig

--- BagiraWebApi/Configs/Auth/AuthConfig.cs
+++ BagiraWebApi/Configs/Auth/AuthConfig.cs
@@ -16,5 +16,15 @@ namespace BagiraWebApi.Configs.Auth
         public required int TokenValidityInMinutes { get; init; }
         [Required]
         public required int RefreshTokenValidityInDays { get; init; }
+        [Required]
+        public required Client[] Clients { get; init; }
+    }
+
+    public class Client
+    {
+        [Required]
+        public required string Id { get; init; }
+        [Required]
+        public required string Secret { get; init; }
     }
 }
