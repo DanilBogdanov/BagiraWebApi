32a1a6d8f0f5e059666fbfdbc2cae622180aad15
DanilWork <bogdanovdanila1404@gmail.com>
2024-07-08 15:35:56 +0500

    refactor: update User model with field code not required

--- BagiraWebApi/Models/Auth/User.cs
+++ BagiraWebApi/Models/Auth/User.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public string? Email { get; set; }
         public string? Phone { get; set; }
-        public required string VerificationCode { get; set; }
+        public string? VerificationCode { get; set; }
         public byte AccessFailedCount { get; set; }
         public bool Confirmed { get; set; }
         public List<Session> Sessions { get; set; } = new();
