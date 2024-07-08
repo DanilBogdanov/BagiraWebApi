c80dfb573fad98859fae091c9ce64e8eb9bb631e
DanilWork <bogdanovdanila1404@gmail.com>
2024-07-08 15:37:10 +0500

    refactor: remove unused forms

--- BagiraWebApi/Services/Auth/DTO/SignInForm.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BagiraWebApi.Services.Auth.DTO
-{
-    public class SignInForm
-    {
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-    }
-}
