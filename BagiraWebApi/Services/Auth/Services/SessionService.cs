7878874c612518842885f196ee3e685ee12b8777
DanilWork <bogdanovdanila1404@gmail.com>
2024-07-12 15:50:36 +0500

    feat: add auth subservices

---
+++ BagiraWebApi/Services/Auth/Services/SessionService.cs
@@ -0,0 +1,72 @@
+﻿using BagiraWebApi.Configs.Auth;
+using BagiraWebApi.Exceptions.Auth;
+using BagiraWebApi.Models.Auth;
+using Microsoft.EntityFrameworkCore;
+
+namespace BagiraWebApi.Services.Auth.Services
+{
+    public class SessionService
+    {
+        private readonly ApplicationContext _context;
+        private readonly AuthConfig _authConfig;
+
+        public SessionService(ApplicationContext context, AuthConfig authConfig)
+        {
+            _context = context;
+            _authConfig = authConfig;
+        }
+
+        public async Task<Session> GetSessionAsync(string sessionId)
+        {
+            var sessionIdGuid = new Guid(sessionId);
+            
+            var session = await _context.Sessions.FirstOrDefaultAsync(x => x.SessionId == sessionIdGuid) 
+                ?? throw new SessionNotFoundException();
+
+            return session;
+        }
+
+        public void CheckSessionToken(Session session, string sessionToken)
+        {
+            var sessionTokenGuid = new Guid(sessionToken);
+
+            if (session.SessionToken != sessionTokenGuid)
+            {
+                throw new SessionValidationException();
+            }
+        }
+
+        public async Task<Session> CreateSessionAsync(User? user)
+        {
+            var session = await _context.Sessions.AddAsync(new Session
+            {
+                User = user,
+                SessionToken = Guid.NewGuid(),
+                Expires = GetSessionExpiresTime(),
+            });
+
+            await _context.SaveChangesAsync();
+
+            return session.Entity;
+        }
+
+        public async Task RefreshSessionAsync(Session session)
+        {
+            session.SessionToken = Guid.NewGuid();
+            session.Expires = GetSessionExpiresTime();
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveSessionAsync(Session session)
+        {
+            _context.Sessions.Remove(session);
+            await _context.SaveChangesAsync();
+        }
+
+        private DateTime GetSessionExpiresTime()
+        {
+            return DateTime.UtcNow.AddDays(_authConfig.RefreshTokenValidityInDays);
+        }
+    }
+}
---
+++ BagiraWebApi/Services/Auth/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using BagiraWebApi.Models.Auth;
+using Microsoft.EntityFrameworkCore;
+
+namespace BagiraWebApi.Services.Auth.Services
+{
+    public class UserService
+    {
+        private readonly ApplicationContext _context;
+
+        public UserService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            var normalizedEmail = NormalizeEmail(email);
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == normalizedEmail);
+
+            return user;
+        }
+
+        public async Task<User> CreateUserByEmailAsync(string login)
+        {
+            var normalizedEmail = NormalizeEmail(login);
+            var user = await _context.Users.AddAsync(
+                new User { Email = normalizedEmail });
+            await _context.SaveChangesAsync();
+
+            return user.Entity;
+        }
+
+        private static string NormalizeEmail(string email)
+        {
+            return email
+                .Trim()
+                .Replace(" ", "")
+                .ToLower();
+        }
+    }
+}
