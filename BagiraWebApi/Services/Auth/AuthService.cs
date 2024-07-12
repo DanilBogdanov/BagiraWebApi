d15ebc3a418d33f96d8db1553e751329f098d763
DanilWork <bogdanovdanila1404@gmail.com>
2024-07-12 15:51:17 +0500

    feat: update auth service with subservices

--- BagiraWebApi/Services/Auth/AuthService.cs
+++ BagiraWebApi/Services/Auth/AuthService.cs
@@ -1,181 +1,94 @@
 ﻿using BagiraWebApi.Configs.Auth;
+using BagiraWebApi.Dtos.Auth;
+using BagiraWebApi.Exceptions.Auth;
 using BagiraWebApi.Models.Auth;
-using BagiraWebApi.Services.Auth.DTO;
-using Microsoft.EntityFrameworkCore;
+using BagiraWebApi.Services.Auth.Services;
+using BagiraWebApi.Services.Messengers;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using BC = BCrypt.Net.BCrypt;
 
 namespace BagiraWebApi.Services.Auth
 {
     public class AuthService
     {
-        private AuthConfig _authConfig;
-        private ApplicationContext _context;
+        private readonly AuthConfig _authConfig;
+        private readonly UserService _userService;
+        private readonly VerifyService _verifyService;
+        private readonly SessionService _sessionService;
+        private readonly TokenService _tokenService;
 
-        public AuthService(IOptions<AuthConfig> options, ApplicationContext context)
+        public AuthService(IOptions<AuthConfig> options, ApplicationContext context, MessengerService messengerService)
         {
             _authConfig = options.Value;
-            _context = context;
+            _userService = new UserService(context);
+            _verifyService = new VerifyService(context, _authConfig, messengerService);
+            _sessionService = new SessionService(context, _authConfig);
+            _tokenService = new TokenService(_authConfig);
         }
 
-        public static TokenValidationParameters GetTokenValidationParameters(AuthConfig config, bool isRefresh = false)
+        public async Task<TokensDto> SignInAnonimous(SignInBaseRequest signInRequest)
         {
-            return new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidIssuer = config.Issuer,
-                ValidateAudience = true,
-                ValidAudience = config.Audience,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(isRefresh ? config.RefreshKey : config.AccessKey))
-            };
-        }
-
-        public async Task<AuthDto> SignIn(SignInForm signInForm)
-        {
-            var user = await _context.Users.SingleAsync(u => u.Email == signInForm.Email);
-
-            if (!VerifyPassword(signInForm.Password, user.Password))
-            {
-                throw new Exception("Wrong password");
-            }
-
-            var session = await CreateSessionAsync(user);
-
-            var tokens = GetTokens(session);
+            var tokens = await SignIn(null, signInRequest);
 
             return tokens;
         }
 
-        public async Task<string> SignUpAsync(SignUpUserForm signUpUserForm)
+        public async Task<TokensDto> SignInEmail(SignInRequest signInRequestDTO)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(user => user.Email == signUpUserForm.Email.ToLower());
+            var user = await _userService.GetUserByEmailAsync(signInRequestDTO.Login)
+                ?? throw new UserNotFoundException();
+            await _verifyService.CheckCode(user, signInRequestDTO.Code);
 
-            if (user != null)
-            {
-                throw new Exception($"User with email:{signUpUserForm.Email} already exist");
-            }
+            var tokens = await SignIn(user, signInRequestDTO);
 
-            var createdUser = await _context.Users.AddAsync(new User
-            {
-                UserName = signUpUserForm.UserName,
-                Email = signUpUserForm.Email.ToLower(),
-                Password = HashPassword(signUpUserForm.Password)
-            });
+            return tokens;
+        }        
 
-            await _context.SaveChangesAsync();
-
-            return createdUser.Entity.Id.ToString();
-        }
-
-        public async Task<AuthDto> RefreshToken(string refreshToken)
+        public async Task<TokensDto> RefreshAsync(ClaimsPrincipal claimsPrincipal)
         {
-            var handler = new JwtSecurityTokenHandler();
-            var tokenValidationResult = await handler.ValidateTokenAsync(
-                refreshToken, GetTokenValidationParameters(_authConfig, true));
-
-            if (!tokenValidationResult.IsValid)
-            {
-                throw new SecurityTokenValidationException();
-            }
-
-            var claims = tokenValidationResult.Claims;
-
-            var sessionIdValue = claims.Single(claim => claim.Key == "sessionId").Value.ToString();
-            var sessionId = Guid.Parse(sessionIdValue);
-            var session = await _context.Sessions
-                .Include(s => s.User)
-                .SingleAsync(s => s.SessionId == sessionId);
-
-            var tokenValue = claims.Single(claim => claim.Key == "token").Value.ToString();
-            var sessionToken = Guid.Parse(tokenValue);
-
-            if (session.SessionToken != sessionToken)
-            {
-                throw new SecurityTokenEncryptionFailedException();
-            }
-
-            session.SessionToken = Guid.NewGuid();
-            session.Expires = DateTime.UtcNow.AddDays(_authConfig.RefreshTokenValidityInDays);
-
-            var tokens = GetTokens(session);
-
-            await _context.SaveChangesAsync();
+            var session = await GetValidSessionAsync(claimsPrincipal);
+            await _sessionService.RefreshSessionAsync(session);
+            var tokens = _tokenService.GetTokens(session);
 
             return tokens;
         }
 
-        private string HashPassword(string password)
+        public async Task SignOut(ClaimsPrincipal claimsPrincipal)
         {
-            string passwordHash = BC.HashPassword(password);
-
-            return passwordHash;
+            var session = await GetValidSessionAsync(claimsPrincipal);
+            await _sessionService.RemoveSessionAsync(session);
         }
 
-        private bool VerifyPassword(string password, string passwordHash)
+        public async Task SendVerificationCodeByEmailAsync(string login)
         {
-            return BC.Verify(password, passwordHash);
+            var user = await _userService.GetUserByEmailAsync(login)
+                ?? await _userService.CreateUserByEmailAsync(login);
+            var code = await _verifyService.UpdateVerificationCodeAsync(user);
+            await _verifyService.SendCodeByEmailAsync(login, code);
         }
 
-        private async Task<Session> CreateSessionAsync(User? user)
+        private async Task<TokensDto> SignIn(User? user, SignInBaseRequest signInRequest)
         {
-            var session = await _context.Sessions.AddAsync(new Session
-            {
-                User = user,
-                SessionToken = Guid.NewGuid(),
-                Expires = DateTime.UtcNow.AddDays(_authConfig.RefreshTokenValidityInDays),
-            });
+            _verifyService.CheckClientIsValid(signInRequest.ClientId, signInRequest.ClientSecret);
+            
+            var session = await _sessionService.CreateSessionAsync(user);
+            var tokens = _tokenService.GetTokens(session);
 
-            await _context.SaveChangesAsync();
-
-            return session.Entity;
+            return tokens;
         }
 
-        private AuthDto GetTokens(Session session)
+        private async Task<Session> GetValidSessionAsync(ClaimsPrincipal claimsPrincipal)
         {
-            var accessClaims = new List<Claim>();
-            if (session.User != null)
-            {
-                accessClaims.Add(new(ClaimTypes.NameIdentifier, session.User.Id.ToString()));
-            }
-
-            var refreshClaims = new List<Claim>
-            {
-                new("sessionId", session.SessionId.ToString()),
-                new("token", session.SessionToken.ToString())
-            };
-
-            var accessLifeTime = TimeSpan.FromMinutes(_authConfig.TokenValidityInMinutes);
-            var refreshLifeTime = TimeSpan.FromDays(_authConfig.RefreshTokenValidityInDays);
-
-            var accessToken = GetToken(accessClaims, _authConfig.AccessKey, accessLifeTime);
-            var refreshToken = GetToken(refreshClaims, _authConfig.RefreshKey, refreshLifeTime);
-
-            return new AuthDto
-            {
-                AccessToken = accessToken,
-                RefreshToken = refreshToken,
-                ExpiresIn = (int)accessLifeTime.TotalSeconds
-            };
-        }
-
-        private string GetToken(List<Claim> claims, string key, TimeSpan lifeTime)
-        {
-            var jwt = new JwtSecurityToken(
-                issuer: _authConfig.Issuer,
-                audience: _authConfig.Audience,
-                claims: claims,
-                expires: DateTime.UtcNow.Add(lifeTime),
-                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(key)), SecurityAlgorithms.HmacSha256));
-
-            return new JwtSecurityTokenHandler().WriteToken(jwt);
+            var sessionId = claimsPrincipal.FindFirstValue("sessionId")
+                ?? throw new ArgumentException("SessionId is required field");
+            var session = await _sessionService.GetSessionAsync(sessionId);
+            var sessionToken = claimsPrincipal.FindFirstValue("sessionToken")
+                ?? throw new ArgumentException("SessionToken is required field");
+            _sessionService.CheckSessionToken(session, sessionToken);
+            
+            return session;
         }
     }
 }
